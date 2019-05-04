c36d0d4d849ac5b484e212f6f79066a41d5960be
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-05-04 08:40:58 +0600

    prepended & appended dates

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,14 +3,34 @@
     <div class="container mt-5">
       <div class="row">
         <div class="col-md-8 offset-2">
-          <div class="text-center">{{ month_str +", " + current_year }}</div>
-          <div class="calendar-container">
+          <div class="row text-center">
+            <div class="col-md-4">
+              <button class="btn btn-success" v-on:click="go_today">today</button>
+            </div>
+            <div class="col-md-4">
+              <div class="month-nav">
+                <button class="btn btn-primary btn-sm" v-on:click="prev_month"><i class="fas fa-caret-left"></i></button>
+                <span class="d-inline-block" style=width:120px>{{ month_str }}</span>
+                <button class="btn btn-primary btn-sm" v-on:click="next_month"><i class="fas fa-caret-right"></i></button>
+              </div>
+              <div class="year-nav mt-2">
+                <button class="btn btn-primary btn-sm" v-on:click="prev_year"><i class="fas fa-caret-left"></i></button>
+                <span class="d-inline-block" style=width:120px>{{ current_year }}</span>
+                <button class="btn btn-primary btn-sm" v-on:click="next_year"><i class="fas fa-caret-right"></i></button>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <!-- <button class="btn btn-success">zz</button> -->
+            </div>
+          </div>
+          <div class="calendar-container mt-2">
             <div class="display-grid grid-col-7 day-container">
               <div v-bind:key="key" v-for="(wk, key) in week_day">{{ wk }}</div>
             </div>
             <div class="display-grid grid-col-7">
-              <div v-bind:key="'m-'+key" v-for="(d, key) in month_starts_at"></div>
+              <div class="text-secondary" v-bind:key="'m-'+key" v-for="(d, key) in prev_month_end_days">{{ d }}</div>
               <div v-bind:key="key" v-for="(d, key) in month_total_days">{{ d }}</div>
+              <div class="text-secondary" v-bind:key="'nm'+key" v-for="(d, key) in next_month_start_days">{{ d }}</div>
             </div>
           </div>
         </div>
@@ -58,10 +78,7 @@ export default {
     return {
       period_count: 8,
       break_after: 4,
-      month_starts_at: 0, //Sunday
       current_date: new Date(),
-      current_month: 0, //January
-      current_year: 1900,
     };
   },
   computed: {
@@ -72,15 +89,63 @@ export default {
       let _t = this;
       let d = new Date(_t.current_year, _t.current_month + 1, 0);
       return d.getDate();
+    },
+    current_month: function () {
+      return this.current_date.getMonth();
+    },
+    current_year: function () {
+      return this.current_date.getFullYear();
+    },
+    month_starts_at: function () {
+      let _t = this;
+      let d = new Date(`${_t.current_year}-${_t.current_month+1}-01`); //vue.warn
+      return d.getDay();
+    },
+    next_month_start_days: function () {
+      let _t = this;
+      let last_day_of_current_month = new Date(_t.current_year, _t.current_month, _t.month_total_days);
+      let month_weeks = Math.ceil((_t.month_total_days + _t.month_starts_at)/7);
+
+      if (month_weeks === 6)
+        return 7 - last_day_of_current_month.getDay() - 1;
+      else if (month_weeks === 5)
+        return 14 - last_day_of_current_month.getDay() - 1; //extended 7 days + remaining weekdays (negative is for getDay index)
+      else if (month_weeks === 4)
+        return 14;  // plain 14, if 4 weeks
+    },
+    prev_month_end_days: function () {
+      let _t = this;
+      let prev_month_total_days = new Date(_t.current_year, _t.current_month, 0).getDate();
+      let prev_month_last_days = [];
+
+      for (let i = prev_month_total_days - _t.month_starts_at; i < prev_month_total_days; i++) {
+        prev_month_last_days.push(i);
+      }
+      // console.log(`Starts: ${_t.month_starts_at}; prev: ${prev_month_last_days}`)
+      return prev_month_last_days;
     }
   },
-  created: function () {
-    let _t = this;
-    _t.current_month = _t.current_date.getMonth();
-    _t.current_year = _t.current_date.getFullYear();
-    let d = new Date(`${_t.current_year}-${_t.current_month+1}-01`); //vue.warn
-    _t.month_starts_at = d.getDay();
-  }
+  methods: {
+    prev_month: function () {
+      let _t = this;
+      _t.current_date = new Date(_t.current_year, _t.current_month - 1, 1);
+    },
+    next_month: function () {
+      let _t = this;
+      _t.current_date = new Date(_t.current_year, _t.current_month + 1, 1);
+    },
+    prev_year: function () {
+      let _t = this;
+      _t.current_date = new Date(_t.current_year - 1, _t.current_month, 1);
+    },
+    next_year: function () {
+      let _t = this;
+      _t.current_date = new Date(_t.current_year + 1, _t.current_month, 1);
+    },
+    go_today: function () {
+      this.current_date = new Date();
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
